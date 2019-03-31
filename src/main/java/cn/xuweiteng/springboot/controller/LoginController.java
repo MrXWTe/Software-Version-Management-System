d533741192ad7e592cdd6a112a1826aa7c575a16
XuWeiteng <XuWeiteng@163.com>
2019-03-31 14:50:28 +0800

    midified Interceptor and finished the login interception function

--- src/main/java/cn/xuweiteng/springboot/controller/LoginController.java
+++ src/main/java/cn/xuweiteng/springboot/controller/LoginController.java
@@ -4,7 +4,6 @@ import cn.xuweiteng.springboot.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpSession;
@@ -13,10 +12,13 @@ import java.util.Map;
 @Controller
 public class LoginController {
 
-    @RequestMapping(value = "/")
+/*
+    //在MyMvcConfig配置了资源访问映射，因此不需要该方法了
+    @RequestMapping(value = {"/", "/login.html"})
     public String login(){
-        return "/login.html";
+        return "login";
     }
+*/
 
     @Autowired
     private LoginService loginService;
@@ -29,8 +31,8 @@ public class LoginController {
 
         boolean flag = loginService.existAdmin(admin_email, admin_password);
         if(flag) {
-            session.setAttribute("admin_email", admin_email);
-            return "/success";
+            session.setAttribute("login_admin", admin_email);
+            return "redirect:/background.html";
         }
         else {
             map.put("errorMessage", "用户名密码错误");
